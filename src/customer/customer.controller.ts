1f72790a80756cc01465c13d07df12be3c2d9cab
jangsen <dh7895210@naver.com>
2025-02-20 16:37:42 +0900

    Add get customer handler

--- src/customer/customer.controller.ts
+++ src/customer/customer.controller.ts
@@ -1,8 +1,11 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { CustomerService } from './customer.service';
 import { ApiConsumes, ApiOperation } from '@nestjs/swagger';
 import { Customer } from './entities/customer.entity';
 import { CreateCustomerDto } from './dto/create-customer.dto';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+
+const GET_CUSTOMER = 'get_customer';
 
 @Controller('customer')
 export class CustomerController {
@@ -15,9 +18,18 @@ export class CustomerController {
     return await this.customerService.createCustomer(dto);
   }
 
-  @Get('/:id')
-  @ApiOperation({ summary: '고객 조회' })
-  async getCustomer(@Param('id') id: string): Promise<Customer> {
-    return await this.customerService.getCustomer(id);
+  // 고객 정보 넘기는 핸들러
+  @MessagePattern(GET_CUSTOMER)
+  async getCustomerHandler(
+    @Payload() data: { customerId: string },
+  ): Promise<Customer> {
+    const { customerId } = data;
+    return await this.customerService.getCustomer(customerId);
   }
+
+  // @Get('/:id')
+  // @ApiOperation({ summary: '고객 조회' })
+  // async getCustomer(@Param('id') id: string): Promise<Customer> {
+  //   return await this.customerService.getCustomer(id);
+  // }
 }
